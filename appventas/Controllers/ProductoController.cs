f2ca88e8875b0f3a9208bca87327c2ccdc29db5f
Eduardo Bustios <eduardo_bustios@usmp.pe>
2021-03-26 02:06:38 -0500

    Creado Index

---
+++ appventas/Controllers/ProductoController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using appventas.Models;
+
+namespace appventas.Controllers
+{
+    public class ProductoController : Controller
+    {
+        private readonly ILogger<ProductoController> _logger;
+
+        public ProductoController(ILogger<ProductoController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+                public IActionResult Create()
+        {
+            return View("Index");
+        }
+    }
+}
