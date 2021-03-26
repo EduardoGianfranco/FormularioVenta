27e90ebdb736875c224c3bda2b62126c1aac9a20
Eduardo Bustios <eduardo_bustios@usmp.pe>
2021-03-25 23:02:51 -0500

    Aplicacion de Ventas

---
+++ appventas/Models/Producto.cs
@@ -0,0 +1,9 @@
+using System;
+
+namespace appventas.Models{
+    public class Producto{
+        public String Nombre{get; set;}
+        public String Precio{get; set;}
+        public String Cantidad{get; set;}
+    }
+}
